806359ac5464c3ae5a8b3df534b034279659bcfb
Jet <jethrohengz@gmail.com>
2023-06-22 15:31:42 +0700

    Added graphql and storage4

--- src/App.js
+++ src/App.js
@@ -17,16 +17,7 @@ import {
   createNote as createNoteMutation,
   deleteNote as deleteNoteMutation,
 } from "./graphql/mutations";
-import { DataStore } from '@aws-amplify/datastore';
-import { Note } from './models';
 
-await DataStore.save(
-  new Note({
-    name: "Lorem ipsum dolor sit amet",
-    description: "Lorem ipsum dolor sit amet",
-    image: "Lorem ipsum dolor sit amet"
-  })
-);
 
 const App = ({ signOut }) => {
   const [notes, setNotes] = useState([]);
